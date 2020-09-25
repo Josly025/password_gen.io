78ca9f317b27f5b0d5d78311668ea4dabd5ac4f2
Grantjos12 <grantjoslyn@gmail.com>
2020-09-24 20:04:35 -0500

    finished if and if else statment and added alert

--- 03_password/Develop/script.js
+++ 03_password/Develop/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 //variable is indexing generate
-// var generateBtn = document.querySelector("#generate");
-// console.log("#generate");
+var generateBtn = document.querySelector("#generate");
+console.log("#generate");
 
 var passLength = prompt(
-  "New password: Choose a length between 8 and 128 characters."
+  "New password: Choose a length between 8 and 128 characters. (Note: leave a space between values)"
 );
 var charType = prompt(
   "Character types to include: lowercase, uppercase, and special."
@@ -25,15 +25,13 @@ var optionsLowerUpper = "adcdefghijklmnopqrstuvwxyzBCDEFGHIJKLMNOPQRSTUVWXYZ";
 var optionsLowerSpecial = "adcdefghijklmnopqrstuvwxyz~!@#$%&-+=_";
 var optionsUpperSpecial = "BCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%&-+=_";
 
-if (charType === ("lowercase" + "uppercase")) {
+if (charType === "lowercase uppercase" || "uppercase lowercase") {
   finalOptions = optionsLowerUpper;
-} else if (charType === ("lowercase" + "special")) {
+} else if (charType === "lowercase special" || "special lowercase") {
   finalOptions = optionsLowerSpecial;
-} else if (charType === ("uppercase" + "special") {
+} else if (charType === "uppercase special" || "special uppercase") {
   finalOptions = optionsUpperSpecial;
-}
-
-if (charType === "lowercase") {
+} else if (charType === "lowercase") {
   finalOptions = optionsLower;
 } else if (charType === "uppercase") {
   finalOptions = optionsUpper;
@@ -46,12 +44,21 @@ console.log(finalOptions); //testing final options
 // var finalOptions;
 
 for (i = 0; i < passLength; i++) {
-  var passNum = Math.floor(Math.random() * passLength + 1);
+  var passNum = Math.floor(Math.random() * passLength);
   ///////
   console.log(passNum); //ensures we are getting # random numbers
   //////
-  passOne += finalOptions.charAt(passNum);
+  passOne += finalOptions.substring(passNum, passNum + 1);
+  //substring returns string while indexing between the string
   /////////charAt returns character at the specified index of the string
-  console.log(passOne);
+  // console.log(passOne);
   // alert("Your new password is: " + passOne); //ensures we are getting a string of passLength with random values
 }
+alert("This is your random password: " + passOne);
+// ..addEventListener("click", passOne);
+
+///Didnt get DOM to work
+var generate = document.getElementById("generate");
+console.log(generate);
+
+generate.children[0].textContent = passOne;
